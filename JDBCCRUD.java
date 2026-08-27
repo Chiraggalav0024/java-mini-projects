6c54a4f15c917e116879311d6ebf59ba1d54ddc3
Chiraggalav0024 <chirag.galav0024@gmail.com>
2026-08-28 04:27:44 +0530

    feat(JDBCCRUD): adding insert query program

---
+++ JDBCCRUD.java
@@ -0,0 +1,25 @@
+
+import java.sql.*;
+
+public class JDBCCRUD{
+    public static void main(String[] args){
+        String url = "jdbc:mariadb://localhost:3306/practice_db";
+        String username = "galav";
+        String passwd = "md@123";
+
+        try{
+        Connection conn = DriverManager.getConnection(url, username, passwd);
+        Statement stmt = conn.createStatement();
+        String sql = "insert into students(id,name,email)" + "values(3,'diggaj','xyz@gmail.com')";
+        int result = stmt.executeUpdate(sql);
+        System.out.println("result:" + result);
+         stmt.close();
+        conn.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+       
+
+    }
+}
