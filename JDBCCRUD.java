e082e99c12c5fdb79ebe6699a78d8937acadc8bf
Chiraggalav0024 <chirag.galav0024@gmail.com>
2026-08-31 15:09:47 +0530

    update(JDBCCRUD): updating program for insert query

--- JDBCCRUD.java
+++ JDBCCRUD.java
@@ -10,7 +10,7 @@ public class JDBCCRUD{
         try{
         Connection conn = DriverManager.getConnection(url, username, passwd);
         Statement stmt = conn.createStatement();
-        String sql = "insert into students(id,name,email)" + "values(9,'dheeraj','xy@gmail.com')";
+        String sql = "insert into students(id,name,email)" + "values(6,'devesh','abc@gmail.com')";
         int result = stmt.executeUpdate(sql);
         System.out.println("result:" + result);
          stmt.close();
